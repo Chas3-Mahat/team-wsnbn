c44ca874703459a4900bdbdcdb8694bb6c5d21f7
Chas3-Mahat <56127094+Chas3-Mahat@users.noreply.github.com>
2019-11-19 19:37:54 +0000

    base files
    basic html files for our pages, and a almost blank javascript file. (unlinked)

---
+++ js/form.js
@@ -0,0 +1,15 @@
+// writes to the log 'hello'
+console.log('hello!')
+
+// Only fires logic after the page has fully loaded
+$( document ).ready(function(){
+
+// this is a one line comment, no exciting javascript yet.
+
+/* so this comment will
+carry over
+between lines
+hahaha */
+
+  })
+})
